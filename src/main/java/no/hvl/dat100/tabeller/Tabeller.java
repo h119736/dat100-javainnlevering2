0c9651d1f962a927d1df6621093a5c0b67e61b1c
haavard-t <havthu89@gmail.com>
2024-09-24 11:26:53 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -5,6 +5,8 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
+		// denne ser grei ut
+
 		System.out.print("[");
 
 		for (int i = 0; i < tabell.length; i++) {
@@ -17,13 +19,30 @@ public class Tabeller {
 	// b)
 	public static String tilStreng(int[] tabell) {
 
+		String txt = "["; // ny kode
 
-		
-		for (int i = 0; i<tabell.length; i++) {
-			System.out.print(tabell[i] + " ");
+		for (int i = 0; i < tabell.length; i++) {
+			// Unødvendig å printe i denne metoden. Strengen må lagres i en variabel
+			// System.out.print(tabell[i] + " "); //original kode
+			if (i == tabell.length - 1) { // ny kode
+				txt += tabell[i]; // ny kode
+			} else { // ny kode
+				txt += tabell[i] + ","; // ny kode
+				// if-else brukes her for å få det rette formatet på strengen ([1,2,3])
+			}
 		}
-		return String ("[" + [i] + "]" );
-		
+
+		txt += "]"; // ny kode
+
+		/*
+		 * [i] er hverken en streng eller en variabel, og kan ikke brukes i en streng.
+		 * ordet String trengs ikke her. Enten returner en strengvariabel, eller
+		 * returner strengen direkte. F.eks: return txt (der txt er en String-variabel
+		 * du allerede har deklarert) ), eller return "teksten du skal returnere her"
+		 */
+		return txt; // ny kode
+		// return String ("[" + [i] + "]" ); //original kode
+
 	}
 
 	// c)
@@ -31,6 +50,8 @@ public class Tabeller {
 
 		// som gitt en tabell av heltall som parameter beregner summen
 		// av tallene som er lagret i tabellen.
+
+		// denne ser grei ut
 
 		int sum = 0;
 
@@ -48,18 +69,23 @@ public class Tabeller {
 		 * tallet tall og false ellers.
 		 */
 
-		int leterEtter = tall;
+		// variabelen leterEtter er unødvendig, man kan bruke variabelen tall direkte
+		// int leterEtter = tall; //original kode
+
 		boolean funnet = false;
 		int i = 0;
 
 		while (!funnet && i < tabell.length) {
-			if (tabell[i] == leterEtter) {
+			if (tabell[i] == tall) {
 				funnet = true;
 			} else {
 				i++;
 			}
-			return funnet;
+			// denne må ut av while-løkken
+			// return funnet; //original kode
 		}
+
+		return funnet;
 
 	}
 
