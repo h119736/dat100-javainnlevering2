4f33f5a7e2d22571b12e47e43926f6d199e452f1
h119736 <119736@stud.hvl.no>
2024-09-26 13:19:12 +0200

    Update Tabeller.java
    Siste versjon. Alt dette virker utenom "erSortert". Får ikke feil på koden, men det dukker opp feil når jeg tester den i JUnit. Kanskje dere ser hva som er feil.

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -17,20 +17,22 @@ public class Tabeller {
 	// b)
 	public static String tilStreng(int[] tabell) {
 
+		String txt = "[";
 
-		
-		for (int i = 0; i<tabell.length; i++) {
-			System.out.print(tabell[i] + " ");
+		for (int i = 0; i < tabell.length; i++) {
+			if (i == tabell.length - 1) {
+				txt += tabell[i];
+			} else {
+				txt += tabell[i] + ",";
+			}
 		}
-		return String ("[" + [i] + "]" );
-		
+		txt += "]";
+		return txt;
+
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
-
-		// som gitt en tabell av heltall som parameter beregner summen
-		// av tallene som er lagret i tabellen.
 
 		int sum = 0;
 
@@ -43,52 +45,87 @@ public class Tabeller {
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
-		/*
-		 * som gitt en tabell og et tall returnerer true om tabellen tabell innholder
-		 * tallet tall og false ellers.
-		 */
-
-		int leterEtter = tall;
 		boolean funnet = false;
 		int i = 0;
 
 		while (!funnet && i < tabell.length) {
-			if (tabell[i] == leterEtter) {
+			if (tabell[i] == tall) {
 				funnet = true;
 			} else {
 				i++;
 			}
-			return funnet;
+
 		}
+		return funnet;
 
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
+		// som returnerer det første index (posisjon) i tabell som inneholder tallet
+		// tall og -1 ellers.
+
+		int i = 0;
+
+		for (i = 0; i < tabell.length;) {
+			if (i == tabell[0]) {
+				i = tabell[0];
+			} else {
+				i = -1;
+			}
+
+		}
+		return i;
+
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden reverser ikke implementert");
+		int[] hjTabell = new int[tabell.length];
+		int n = tabell.length;
+
+		for (int i = n - 1; i > -1; i--) {
+			hjTabell[n - i - 1] = tabell[i];
+
+		}
+		return tabell;
 	}
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erSortert ikke implementert");
+		boolean sortert = false;
+
+		for (int i = 0; i < tabell.length; i++) {
+			if (tabell[i] > tabell[i + 1]) {
+				sortert = false;
+			} else {
+				sortert = true;
+			}
+		}
+		return sortert;
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden settSammen ikke implementert");
+		// som gitt to tabeller tabell1 og tabell2 som parametre,
+		// returnerer en ny tabell som er sammensetning av de to tabellene gitt med som
+		// parameter.
 
+		int t1 = tabell1.length;
+		int t2 = tabell2.length;
+		int t3 = t1 + t2;
+		int[] tabell3 = new int[t3];
+
+		for (int i = 0; i < tabell1.length; i++) {
+			tabell3[i] = tabell1[i];
+		}
+		for (int i = 0; i < tabell2.length; i++) {
+			tabell3[i] = tabell2[i];
+		}
+		return tabell3;
 	}
 }
