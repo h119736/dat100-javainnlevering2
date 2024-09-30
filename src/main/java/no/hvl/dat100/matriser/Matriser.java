fe589c8cb7bc403945e26510ba38fdb3cce2003b
h119736 <119736@stud.hvl.no>
2024-09-30 11:13:43 +0200

    Update Matriser.java
    Oppgave a) og d) funker. Oppgave b) tilStreng funker nesten, men jeg ser ikke det siste steget.
    
    De andre står jeg helt fast på.

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -4,42 +4,105 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+
+		for (int[] rad : matrise) {
+			for (int x : rad) {
+				System.out.print(x + " ");
+			}
+			System.out.println();
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-		
+		// som returnerer en streng-representation av en matrise. Om matrisen er
+		// følgende:
+		// { {1,2,3}, {4,5,6}, {7,8,9} }
+		// 1 2 3 \n4 5 6 \n7 8 9 \n
+
+		String tallTxt = null;
+		String tallTxt1 = null;
+		String a = "\n";
+
+		for (int[] rad : matrise) {
+			for (int x : rad) {
+				tallTxt = " " + x;
+			}
+			tallTxt1 = tallTxt + a;
+		}
+		return tallTxt1;
+
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
+
+		/*
+		 * som returnerer en ny matrise der alle tall i matrisen er multiplisert med
+		 * parameteren tall. Metoden må først opprette en ny matrise (fler-dimensjonell
+		 * tabell) som er like stor som parameteren og så multiplisere alle elementer
+		 * med tall.
+		 * 
+		 */
 	
+		int[][] matriseHjelp = matrise;
+	
+		
+		for(int i =0; i < matrise.length; i++) {
+			for(int j =0; j < matrise.length; j++) {
+				matriseHjelp = tall * [j];
+				matrise [i][j]= matriseHjelp;
+			}
+		} return matrise;
+		
+//		for (int[] rad : matrise) {
+//			for(int x : rad) {
+//				int y = x*tall;
+//				int [] hMAtriseRad = y;
+//			}
+//		} matrise = hMatrise;
+//				return matrise;
+
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
+		// som avgjør om to matriser gitt ved parametrene a og b er like
+
+		boolean like = false;
+
+		int rad1 = a.length;
+		int kolonne1 = a[0].length;
+
+		int rad2 = b.length;
+		int kolonne2 = b[0].length;
 		
+		if(rad1 != rad2 || kolonne1 != kolonne2) {
+			return like;
+		}else {
+			for (int i = 0; i < rad1; i++) {
+			for (int j = 0; j < kolonne1; j++) {
+				if(a[i][j] != b[i][j]) {
+					return like;
+				}else {
+					like = true;
+				}
+			
+			}
+		}
+
+	}return like;
 	}
-	
+
 	// e)
 	public static int[][] speile(int[][] matrise) {
 
 		// TODO
 
 		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-	
+
 	}
 
 	// f)
@@ -47,6 +110,6 @@ public class Matriser {
 
 		// TODO
 		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-	
+
 	}
 }
